7ac9e215a4c6eaf707da8310a26bc3bf2fd08748
Mahmud Hasan <mahmud.hasan.buet15@gmail.com>
2021-09-01 22:43:36 +0600

    adding more files to repository

---
+++ Problem_set_13(#8_week_sql_challenge)/part_A/part_A.sql
@@ -0,0 +1,180 @@
+USE pizza_runner;
+
+############ Part A - Pizza Metrics ######
+
+-- 1. How many pizzas were ordered?
+
+SELECT COUNT(*) AS  pizza_ordered
+FROM customer_orders
+;
+
+
+-- 2. How many unique customer orders were made?
+
+SELECT COUNT(DISTINCT order_id) AS unique_customer_orders
+FROM customer_orders
+;
+
+
+-- 3. How many successful orders were delivered by each runner?
+
+SELECT runner_id,COUNT(*) AS successfull_orders
+FROM customer_orders c
+JOIN runner_orders ro ON c.order_id=ro.order_id
+WHERE pickup_time <> 'null'      -- As the datatype is VARCHAR, IS NOT NULL shouldn't be used.
+GROUP BY 1
+;
+
+
+-- 4. How many of each type of pizza was delivered?
+
+#method- 1
+
+SELECT 
+    CASE
+        WHEN pizza_id = 1 THEN 'Meat Lovers'
+        ELSE 'Vegetarian'
+    END AS pizza_types,
+    COUNT(*) AS order_count
+FROM
+    customer_orders c
+        JOIN
+    runner_orders ro ON c.order_id = ro.order_id
+WHERE
+    pickup_time <> 'null'
+GROUP BY 1
+;
+
+#method- 2
+
+SELECT 
+    SUM(CASE
+        WHEN pizza_id = 1 THEN 1
+        ELSE 0
+    END) AS 'Meat Lovers',
+    SUM(CASE
+        WHEN pizza_id = 2 THEN 1
+        ELSE 0
+    END) AS 'Vegetarian'
+FROM
+    customer_orders c
+        JOIN
+    runner_orders ro ON c.order_id = ro.order_id
+WHERE
+    pickup_time <> 'null'
+;
+
+
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT 
+    customer_id,
+    SUM(CASE
+        WHEN pizza_id = 1 THEN 1
+        ELSE 0
+    END) AS 'Meat Lovers',
+    SUM(CASE
+        WHEN pizza_id = 2 THEN 1
+        ELSE 0
+    END) AS 'Vegetarian'
+FROM
+    customer_orders
+GROUP BY 1
+;
+
+
+-- 6. What was the maximum number of pizzas delivered in a single order?
+
+SELECT COUNT(*) AS max_num_of_delivery
+FROM
+    customer_orders c
+        JOIN
+    runner_orders ro ON c.order_id = ro.order_id
+WHERE
+    pickup_time <> 'null'
+GROUP BY c.order_id
+ORDER BY max_num_of_delivery DESC
+LIMIT 1
+;
+
+
+-- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT 
+       CASE
+         WHEN exclusions REGEXP '[0-9]' OR extras REGEXP '[0-9]' THEN 'has change'
+         ELSE 'no change'
+	   END AS change_or_not,
+       COUNT(*) AS pizza_count
+FROM
+    customer_orders c
+        JOIN
+    runner_orders ro ON c.order_id = ro.order_id
+WHERE
+    pickup_time <> 'null'
+    AND extras <> 'null' 
+GROUP BY 1
+ORDER BY 2 DESC
+;
+
+
+WITH CTE
+AS (SELECT customer_id,
+		   exclusions,
+		   extras,
+		   CASE
+			 WHEN exclusions REGEXP '[0-9]' OR extras REGEXP '[0-9]' THEN 1 ELSE 0
+		   END AS changes
+	FROM
+		customer_orders c
+			JOIN
+		runner_orders ro ON c.order_id = ro.order_id
+	WHERE
+		pickup_time <> 'null'
+		AND extras <> 'null' )
+
+SELECT customer_id,
+       SUM(changes) AS  has_changes,
+       SUM(CASE
+		 WHEN changes = 0 THEN 1 ELSE 0
+	   END) AS no_changes
+FROM CTE
+GROUP BY 1
+
+
+;
+
+
+-- 8. How many pizzas were delivered that had both exclusions and extras?
+
+SELECT COUNT(*) AS pizzas_with_exclusions_and_extras
+FROM
+    customer_orders c
+        JOIN
+    runner_orders ro ON c.order_id = ro.order_id
+WHERE
+    pickup_time <> 'null'
+    AND exclusions REGEXP '[0-9]' AND extras REGEXP '[0-9]'
+;
+
+
+-- 9. What was the total volume of pizzas ordered for each hour of the day?
+
+SELECT 
+       HOUR(CAST(order_time AS DATETIME)) AS hour,
+       COUNT(*) AS order_count
+FROM customer_orders
+GROUP BY 1
+;
+
+
+-- 10. What was the volume of orders for each day of the week?
+
+SELECT 
+       DAYNAME(CAST(order_time AS DATETIME)) AS day_of_week,
+       COUNT(*) order_count
+       
+FROM customer_orders
+GROUP BY 1
+ORDER BY 2 DESC
+;
---
+++ Problem_set_13(#8_week_sql_challenge)/part_C/part_C.sql
@@ -0,0 +1,167 @@
+USE pizza_runner;
+
+############ Part C. Ingredient Optimisation ######
+
+-- 1. What are the standard ingredients for each pizza?
+
+
+
+-- 2. What was the most commonly added extra?
+# Step-1. Separate comma separated value in different rows. Use SUBSTRING_INDEX(str,delim,count)
+# step-2. Count the occurrences for each item.
+# step-3. Find the name of that item.
+
+# experimenting with SUBSTRING_INDEX()
+SELECT extras,
+       SUBSTRING_INDEX(extras,',',1),   -- first value
+       SUBSTRING_INDEX(SUBSTRING_INDEX(extras,',',2),',',-1)    -- last value
+FROM customer_orders
+WHERE extras REGEXP '[0-9]+' 
+;
+
+# finding a way to create multiple row
+WITH CTE 
+AS (SELECT 1 AS n
+	  UNION SELECT 2 AS n
+	  UNION SELECT 3 AS n
+)
+
+SELECT *
+FROM CTE
+;
+
+
+
+# Final solution
+
+WITH CTE 
+AS (SELECT 1 AS n
+	  UNION SELECT 2 AS n
+	  UNION SELECT 3 AS n
+),
+
+CTE2
+AS (SELECT extras,
+            n,
+			CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(extras,',',n),',',-1) AS FLOAT) AS extras_mod
+	FROM customer_orders
+	JOIN CTE ON CHAR_LENGTH(extras)-CHAR_LENGTH(REPLACE(extras,',',""))>=n-1      -- advanced join
+	WHERE extras REGEXP '[0-9]+') ,
+
+CTE3
+AS (SELECT extras_mod,COUNT(*) AS occurrences
+	FROM CTE2
+	GROUP BY 1
+	ORDER BY 2 DESC
+	LIMIT 1)
+    
+SELECT topping_name AS most_commonly_added_extra
+FROM pizza_toppings
+WHERE topping_id=( SELECT extras_mod
+                   FROM CTE3)
+;
+
+
+-- # for understanding
+
+-- WITH CTE 
+-- AS (SELECT 1 AS n
+-- 	  UNION SELECT 2 AS n
+-- 	  UNION SELECT 3 AS n
+-- )
+
+-- SELECT      *,
+--            
+--             n,
+--             CHAR_LENGTH(extras)-CHAR_LENGTH(REPLACE(extras,',',"")),
+--             extras,
+--             SUBSTRING_INDEX(extras,',',n),
+--             
+-- 			CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(extras,',',n),',',-1) AS FLOAT) AS extras_mod
+-- 	FROM customer_orders
+-- 	JOIN CTE ON CHAR_LENGTH(extras)-CHAR_LENGTH(REPLACE(extras,',',""))>=n-1     -- advanced join
+-- 	WHERE extras REGEXP '[0-9]+'
+-- ;
+
+
+
+-- 3. What was the most common exclusion?
+
+# Step-1. Separate comma separated value in different rows. Use SUBSTRING_INDEX(str,delim,count)
+# step-2. Count the occurrences for each item.
+# step-3. Find the name of that item.
+
+WITH CTE
+AS (SELECT 1 AS n
+	UNION SELECT 2 AS  n
+	UNION SELECT 3 AS n)
+ ,
+CTE2 
+AS (SELECT order_id,
+		   n,
+		   exclusions,
+		   CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(exclusions,',',n),',',-1) AS FLOAT) AS exclusions_mod
+		   
+	FROM CTE
+	JOIN customer_orders
+	ON CHAR_LENGTH(exclusions)-CHAR_LENGTH(REPLACE(exclusions,',',''))>=n-1
+	WHERE exclusions REGEXP '[0-9]+' )
+ ,
+CTE3
+AS (SELECT exclusions_mod,COUNT(*) AS occurrences
+	FROM CTE2
+	GROUP BY 1
+	ORDER BY 2 DESC
+	LIMIT 1)
+
+SELECT topping_name AS most_common_exclusion
+FROM pizza_toppings
+WHERE topping_id= (SELECT exclusions_mod
+                   FROM CTE3)
+;
+
+SELECT exclusions,
+       CHAR_LENGTH(exclusions),
+       CHAR_LENGTH(REPLACE(exclusions,',','')),
+       CHAR_LENGTH(exclusions)-CHAR_LENGTH(REPLACE(exclusions,',',''))
+FROM customer_orders
+WHERE exclusions REGEXP '[0-9]+' 
+;
+
+
+
+
+
+-- 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
+--    a. Meat Lovers
+--    b. Meat Lovers - Exclude Beef
+--    c. Meat Lovers - Extra Bacon
+--    d. Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+
+SELECT *,
+       CASE 
+         WHEN pizza_id=1 THEN 'Meat Lovers'
+         ELSE 'Vegetarian'
+	   END AS pizza_type
+FROM customer_orders
+;
+
+SELECT *
+FROM pizza_toppings
+;
+
+SELECT exclusions,
+        CAST(exclusions AS FLOAT)
+FROM customer_orders
+;
+
+
+-- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+--    For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+
+
+
+
+-- 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
+
+
